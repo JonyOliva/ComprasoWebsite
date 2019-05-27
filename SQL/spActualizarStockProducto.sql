716ef67f92d0d64f5dcbba569db7d8e8c177391d
OlivaJony <olivajonatan.sp@gmail.com>
2019-05-27 19:06:46 -0300

    agregue el script de la bd

--- SQL/spActualizarStockProducto.sql
+++ SQL/spActualizarStockProducto.sql
@@ -1,15 +1,8 @@
-CREATE PROCEDURE spEliminarProducto
+CREATE PROCEDURE spActualizarStockProducto
 (
 @IDProducto char(10),
 @Nombre char(10),
-@Cat char(10),
-@SubCat char(10),
-@Marca char(10),
-@Descrip text,
-@FichaTec char(50),
-@Stock float,
-@Precio float,
-@Descuento float
+@Stock float
 )
 AS
 IF((SELECT Stock FROM PRODUCTOS WHERE IDProducto=@IDProducto) <= 1)
