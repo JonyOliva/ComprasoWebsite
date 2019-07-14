e72d04d9053460b7cf5344426fcb9c6def76a093
facuv <facv.velazco@gmail.com>
2019-07-14 19:19:22 -0300

    prueben

--- ArvoProjectWebsite/WebForms/frmMenuAdmin.aspx.cs
+++ ArvoProjectWebsite/WebForms/frmMenuAdmin.aspx.cs
@@ -14,7 +14,6 @@ namespace ArvoProjectWebsite
     public partial class frmMenuAdmin : System.Web.UI.Page
     {
 
-        static int index = -1;
         protected void Page_Load(object sender, EventArgs e)
         {
            
@@ -50,15 +49,13 @@ namespace ArvoProjectWebsite
                 ddlBuscarMarcas.Items.Insert(0, new ListItem("", null));
             }
             gestionProductos gp = new gestionProductos();
-            if (index != -1)
+            if (grdProd.EditIndex != -1)
             {
-                string cat = ((DropDownList)grdProd.Rows[index].FindControl("ddlCatEdit")).SelectedValue;
-                ((DropDownList)grdProd.Rows[index].FindControl("ddlSubcatEdit")).DataSource =
+                string cat = ((DropDownList)grdProd.Rows[grdProd.EditIndex].FindControl("ddlCatEdit")).SelectedValue;
+                ((DropDownList)grdProd.Rows[grdProd.EditIndex].FindControl("ddlSubcatEdit")).DataSource =
                     gp.getListaSubCategorias(cat);
-                ((DropDownList)grdProd.Rows[index].FindControl("ddlSubcatEdit")).DataBind();
-
+                ((DropDownList)grdProd.Rows[grdProd.EditIndex].FindControl("ddlSubcatEdit")).DataBind();
             }
-            
         }
 
         protected void btnProductos_Click(object sender, EventArgs e)
@@ -152,7 +149,6 @@ namespace ArvoProjectWebsite
         protected void grdProd_RowEditing(object sender, GridViewEditEventArgs e)
         {
             grdProd.EditIndex = e.NewEditIndex;
-            index = grdProd.EditIndex;
             cargarGridViewProd();
             gestionProductos gp = new gestionProductos();
         }
